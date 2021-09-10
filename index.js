847e9ecff54540d72b567b494d29fb8b0c45573a
alefnull <nullalef@gmail.com>
2021-09-10 16:56:39 -0400

    removed strict statement; disabled jshint rule

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { yellow } = require('chalk');
 const plist_id = process.argv[2];
 console.log(yellow('\nfetching playlist and sorting by upload date. please hold...'));
 (async () => {
-	'use strict';
 	const playlist = await usetube.getPlaylistVideos(plist_id);
 	const videos = playlist.sort((a, b) => {
 		return a.publishedAt > b.publishedAt ? 1 : -1;
